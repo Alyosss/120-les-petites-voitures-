42a0f0895bd89017c11360635bc3d37ee961c56b
alyosss <aloys.gallo@ensae.fr>
2024-10-22 08:54:09 +0000

    diuiedieziekzj

---
+++ aloysgallo.py
@@ -0,0 +1,5 @@
+"""
+J'aime les licornes et leurs jolies cornes, elles ont un pelage comme un beau nuage. 
+Elles sont si gentilles avec leurs amiss, même si elles pratiquent la sodomie. 
+Le grand JD
+"""
